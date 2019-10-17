5a4a589f0a404f5e094986fa9f0bd40fb59c2365
HugoAR8 <50413555+HugoAR8@users.noreply.github.com>
2019-10-17 19:37:50 -0300

    Add files via upload

---
+++ Paciente.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Paciente{
+  private int saúde;
+  private int horário;
+  private string necessidade;
+  public Paciente(int s, int h, string n){
+   saúde = s;
+   horário = h;
+   necessidade = n;
+  }
+  
+}
