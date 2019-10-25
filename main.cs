e1fc1a8fc3a0429f4e3a96984b7782c331eeec73
maxdu1998 <50428941+maxdu1998@users.noreply.github.com>
2019-10-25 16:11:21 -0300

    Add files via upload

--- main.cs
+++ main.cs
@@ -1,43 +1,176 @@
 using System;
-namespace Sistema{
+namespace Sistema {
 
-class MainClass {
-  public static void Main (string[] args) {
-    DB db = new DB();
+ class MainClass {
+  public static void Main(string[] args) {
+
+   menu();
+
+  }
+
+  public static void menu() {
+   string aux;
+   DB db = new DB();
+   Console.Clear();
+   art();
+
+   Console.WriteLine("Pacientes");
+   Console.WriteLine("Consultas");
+   Console.WriteLine("Médicos");
+   aux = Console.ReadLine();
+
+   if (aux == "Pacientes") {
     Console.Clear();
-    string aux;
-
-    Console.WriteLine("Pacientes");
-    Console.WriteLine("Consultas");
-    Console.WriteLine("Médicos");
-    
-
-    aux = (Console.ReadLine());
-
-    if(aux == "Pacientes"){
-    Console.Clear();  
-
-    Console.WriteLine("Cadastre os pacientes");
-    db.cadastrarPacientes();
-
+    Console.WriteLine("Cadastrar");
     Console.WriteLine("Listar");
     Console.WriteLine("Buscar");
+    Console.WriteLine("Editar");
+    Console.WriteLine("Excluir");
+    Console.WriteLine("Menu");
+
     aux = (Console.ReadLine());
-    Console.Clear();
+
+    if (aux == "Cadastrar") {
+     Console.Clear();
+     db.cadastrarPaciente();
+
+    } else {
+     if (aux == "Listar") {
+      Console.Clear();
+      db.listarPacientes();
+
+     } else {
+      if (aux == "Buscar") {
+       Console.Clear();
+       // db.buscarPaciente(0);
+
+      } else {
+       if (aux == "Editar") {
+        Console.Clear();
+        db.editarPaciente();
+
+       } else {
+        if (aux == "Excluir") {
+         Console.Clear();
+         db.excluirPaciente();
+
+        } else {
+         menu();
+        }
+
+       }
+      }
+     }
+    }
+
+   } else {
+    if (aux == "Consultas") {
+     Console.Clear();
+     Console.WriteLine("Marcar");
+     Console.WriteLine("Listar");
+     Console.WriteLine("Buscar");
+     Console.WriteLine("Editar");
+     Console.WriteLine("Excluir");
+     Console.WriteLine("Menu");
+     aux = (Console.ReadLine());
+
+     if (aux == "Marcar") {
+      Console.Clear();
+      db.marcarConsulta();
+
+     } else {
+      if (aux == "Listar") {
+       Console.Clear();
+       db.listarPacientes();
+
+      } else {
+       if (aux == "Buscar") {
+        Console.Clear();
+        //db.buscarPaciente();
+
+       } else {
+        if (aux == "Editar") {
+         Console.Clear();
+         db.editarPaciente();
+
+        } else {
+         if (aux == "Excluir") {
+          Console.Clear();
+          db.excluirPaciente();
+
+         } else {
+          menu();
+         }
+
+        }
+       }
+      }
+     }
+
+    } else {
+     if (aux == "Médicos") {
+      Console.Clear();
+      Console.WriteLine("Cadastrar");
+      Console.WriteLine("Listar");
+      Console.WriteLine("Buscar");
       Console.WriteLine("Editar");
       Console.WriteLine("Excluir");
+      Console.WriteLine("Menu");
       aux = (Console.ReadLine());
+
+      if (aux == "Cadastrar") {
+       Console.Clear();
+       // db.cadastrarMedico();
+
+      } else {
+       if (aux == "Listar") {
+        Console.Clear();
+        // db.listarMedicos();
+
+       } else {
+        if (aux == "Buscar") {
+         Console.Clear();
+         //  db.buscarMedico();
+
+        } else {
+         if (aux == "Editar") {
+          Console.Clear();
+          // db.editarMedico();
+
+         } else {
+          if (aux == "Excluir") {
+           Console.Clear();
+           //db.excluirMedico();
+
+          } else {
+           menu();
+          }
+         }
+        }
+
+
+       }
+      }
+     }
+
 
     }
 
+    menu();
 
+   }
+  }
 
-  
+  public static void art() {
 
-
-    
-
-
-
-  }}
+    Console.WriteLine(@"  
+  __  __          _   ____  _           
+ |  \/  | ___  __| | |  _ \| |_   _ ___ 
+ | |\/| |/ _ \/ _` | | |_) | | | | / __|
+ | |  | |  __/ (_| | |  __/| | |_| \__ \
+ |_|  |_|\___|\__,_| |_|   |_|\__,_|___/
+                                        ");
+                                        
+  }
+ }
 }
