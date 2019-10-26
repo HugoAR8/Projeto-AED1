980433fa5b8e42d8acd48ec784291a483ce30f7c
maxdu1998 <50428941+maxdu1998@users.noreply.github.com>
2019-10-25 23:07:56 -0300

    Update DB.cs

--- DB.cs
+++ DB.cs
@@ -22,28 +22,6 @@ namespace Sistema {
    Console.Write("Lista de Pacientes:\n");
    Console.Write("\n");
 
-   //--------------- teste
-   Paciente p1 = new Paciente();
-   p1.setNome("a");
-   pacientes[0] = p1;
-
-   Paciente p2 = new Paciente();
-   p2.setNome("Teste2");
-   pacientes[2] = p2;
-
-   Paciente p3 = new Paciente();
-   p3.setNome("Teste3");
-   pacientes[3] = p3;
-
-   Paciente p4 = new Paciente();
-   p4.setNome("Teste4");
-   pacientes[4] = p4;
-
-   Paciente p5 = new Paciente();
-   p5.setNome("Teste5");
-   pacientes[5] = p5;
-   //---------------------
-
    for (int i = 0; i < pacientes.Length; i++) {
     if (pacientes[i] != null) {
      aux = pacientes[i];
@@ -52,54 +30,34 @@ namespace Sistema {
     }
    }
   }
-
+  
+  
   public void buscarPaciente(string nome) {
-   Paciente aux = new Paciente();
+    Paciente aux = new Paciente();
 
-   //--------------- teste
-
-  Paciente p1 = new Paciente();
-   p1.setNome("a");
-   pacientes[0] = p1;
-
-   Paciente p2 = new Paciente();
-   p2.setNome("b");
-   pacientes[2] = p2;
-
-   Paciente p3 = new Paciente();
-   p3.setNome("c");
-   pacientes[3] = p3;
-
-   Paciente p4 = new Paciente();
-   p4.setNome("d");
-   pacientes[4] = p4;
-
-   Paciente p5 = new Paciente();
-   p5.setNome("e");
-   pacientes[5] = p5;
-   //---------------------
+  
 
    for (int i = 0; i < pacientes.Length; i++) {
-  
-    if (pacientes[i] != null) {
-     aux = pacientes[i];
+    
+    aux = pacientes[i];
 
-     if (String.Equals(aux.getNome(), nome)) {
-       Console.Clear();
-      Console.Write("Nome: {0}", aux.getNome());
-      Console.Write("\n");
-      Console.Write("Idade: {0}", aux.getIdade());
-      Console.Write("\n");
-      Console.Write("Peso: {0}", aux.getPeso());
-      Console.Write("\n");
-      Console.Write("Altura: {0}", aux.getAltura());
-      Console.Write("\n");
 
-      break;
+    if (String.Equals(aux.getNome(), nome)) {
+     Console.Clear();
+     Console.Write("Nome: {0}", aux.getNome());
+     Console.Write("\n");
+     Console.Write("Idade: {0}", aux.getIdade());
+     Console.Write("\n");
+     Console.Write("Peso: {0}", aux.getPeso());
+     Console.Write("\n");
+     Console.Write("Altura: {0}", aux.getAltura());
 
-     } 
-    } 
-   } 
+     break;
+
+   }
+    Console.Write("false");
+
+   }
 
   }
 
@@ -114,33 +72,54 @@ namespace Sistema {
    
  }
 
-  public void editarPaciente() {
+public void editarPaciente(string nome) {
+   Paciente aux = new Paciente();
+
+   for (int i = 0; i < pacientes.Length; i++) {
+    aux = pacientes[i];
+
+    if (pacientes[i] != null) {
+
+     if (String.Equals(aux.getNome(), nome)) {
+      Console.WriteLine("Editar nome do paciente: ");
+      aux.setNome(Console.ReadLine());
+      Console.WriteLine("Editar idade do paciente: ");
+      aux.setIdade(int.Parse(Console.ReadLine()));
+      Console.WriteLine("Editar altura do paciente: ");
+      aux.setAltura(double.Parse(Console.ReadLine()));
+      Console.WriteLine("Editar peso do paciente: ");
+      aux.setPeso(double.Parse(Console.ReadLine()));
+
+      pacientes[i] = aux;
+      break;
+     }
+    }
+
+   }
+
 
   }
 
   public void cadastrarPaciente() {
-    
-    Paciente aux = new Paciente();
+   Paciente aux = new Paciente();
 
-    Console.WriteLine("Escreva o nome do paciente: ");
-    aux.setNome(Console.ReadLine());
+   Console.WriteLine("Escreva o nome do paciente: ");
+   aux.setNome(Console.ReadLine());
+   Console.WriteLine("Escreva a idade do paciente: ");
+   aux.setIdade(int.Parse(Console.ReadLine()));
+   Console.WriteLine("Escreva a altura do paciente: ");
+   aux.setAltura(double.Parse(Console.ReadLine()));
+   Console.WriteLine("Escreva o peso do paciente: ");
+   aux.setPeso(double.Parse(Console.ReadLine()));
 
-    Console.WriteLine("Escreva a idade do paciente: ");
-    aux.setIdade(int.Parse(Console.ReadLine()));
+   for (int i = 0; i < pacientes.Length; i++) {
 
-    Console.WriteLine("Escreva a altura do paciente: ");
-    aux.setAltura(double.Parse(Console.ReadLine()));
+    if (pacientes[i] == null) {
+     pacientes[i] = aux;
+     break;
 
-    Console.WriteLine("Escreva o peso do paciente: ");
-    aux.setPeso(double.Parse(Console.ReadLine()));
-
-    for (int i = 0; i < pacientes.Length; i++){
-     if(pacientes[i] == null){
-      pacientes[i] = aux;
-     }
     }
-
-   
+   }
   }
 
 
